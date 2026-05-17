b07c85f65297fed29ca0890395ed920c98da1e8c
Ori <redraino.o.o.o.o@gmail.com>
2026-05-17 07:39:58 -0400

    prep data attachment

--- src/main/java/io/github/redrain0o0/globaladvancements/Globaladvancements.java
+++ src/main/java/io/github/redrain0o0/globaladvancements/Globaladvancements.java
@@ -1,17 +1,29 @@
 package io.github.redrain0o0.globaladvancements;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.function.Supplier;
 
 public class Globaladvancements implements ModInitializer {
     public static final String MOD_ID = "globaladvancements";
     public static final String MOD_NAME = "Global Advancements";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
+    public static final AttachmentType<Boolean> HAS_MOD = AttachmentRegistry.create(
+            createId("hasMod")
+    );
+
     @Override
     public void onInitialize() {
+        ServerPlayerEvents.JOIN.register((player) -> {
+            player.setAttached(HAS_MOD, true);
+        });
     }
 
     public static Identifier createId(String id) {
